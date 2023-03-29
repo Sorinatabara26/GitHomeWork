b536cbf168d07efa3be3fa086a2e472cd233bdf7
Sorinatabara26 <tabori.sorina@gmail.com>
2023-03-29 11:04:53 -0500

    my firt commit

---
+++ src/Task1.java
@@ -0,0 +1,19 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+         /* Create a repo on local, have a basic code in it for printing integers from 1 to 10
+        Push it to git hub.
+
+       Step2
+        Clone the pushed repo as being a member of a project.
+        Update the code and make it print integers till 100.
+        Push the code to the same repo*/
+
+
+
+                for (int i = 0; i <= 10; i++) {
+                    System.out.println(i);
+                }
+
+            }
+}
